2b1c6370d4c99b7f2f9f1b8a80b7caa9ae5d2720
Nayan <nayan.kadam1307@gmail.com>
2020-12-07 23:19:55 +0530

    NYN_SPRING_Security classes created

---
+++ SpringBoot_Security_JWT/src/main/java/com/springbootjwt/SpringBoot_Security_JWT/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.springbootjwt.SpringBoot_Security_JWT.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello Word"; 
+	}
+}
