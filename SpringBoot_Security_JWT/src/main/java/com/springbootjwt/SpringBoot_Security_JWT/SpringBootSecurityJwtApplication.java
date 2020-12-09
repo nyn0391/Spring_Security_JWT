2d8d5489ee1e45c733177eb8ca926121f3a3afe5
Nayan <nayan.kadam1307@gmail.com>
2020-12-09 23:08:31 +0530

    NYN_1_0_0 Removed Space

--- SpringBoot_Security_JWT/src/main/java/com/springbootjwt/SpringBoot_Security_JWT/SpringBootSecurityJwtApplication.java
+++ SpringBoot_Security_JWT/src/main/java/com/springbootjwt/SpringBoot_Security_JWT/SpringBootSecurityJwtApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
-
- 
 	public static void main(String[] args) { 
 		System.out.println("main");
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
