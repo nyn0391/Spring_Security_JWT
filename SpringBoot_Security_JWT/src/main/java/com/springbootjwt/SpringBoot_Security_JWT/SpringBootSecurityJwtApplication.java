95122b4331648888b9828bb22648b601bed4b411
Nayan <nayan.kadam1307@gmail.com>
2020-12-09 23:04:54 +0530

    Test

--- SpringBoot_Security_JWT/src/main/java/com/springbootjwt/SpringBoot_Security_JWT/SpringBootSecurityJwtApplication.java
+++ SpringBoot_Security_JWT/src/main/java/com/springbootjwt/SpringBoot_Security_JWT/SpringBootSecurityJwtApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSecurityJwtApplication {
 
-
+ 
 	public static void main(String[] args) { 
 		System.out.println("main");
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
